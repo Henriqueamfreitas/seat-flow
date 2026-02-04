52ea9ba5aa5820c473f06e2c6ba2ed682d62af84
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2026-02-04 20:33:51 -0300

    middlewares no seat e reservaion

--- reservation-service/src/infra/http/routes/reservation.routes.ts
+++ reservation-service/src/infra/http/routes/reservation.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { CreateReservationController } from "../controllers/reservation/create-reservation.controller";
 import { CancelReservationController } from "../controllers/reservation/cancel-reservation.controller";
 import { ListReservationsController } from "../controllers/reservation/list-reservations.controller";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const reservationRoutes = Router();
 
-reservationRoutes.post("/", new CreateReservationController().handle);
-reservationRoutes.post("/:id/cancel", new CancelReservationController().handle);
-reservationRoutes.get("/", new ListReservationsController().handle);
+reservationRoutes.post("/", [ensureAuthenticated], new CreateReservationController().handle);
+reservationRoutes.post("/:id/cancel", [ensureAuthenticated], new CancelReservationController().handle);
+reservationRoutes.get("/", [ensureAuthenticated, ensureAdmin], new ListReservationsController().handle);
 
 export default reservationRoutes;
--- seat-service/src/infra/http/routes/seat.routes.ts
+++ seat-service/src/infra/http/routes/seat.routes.ts
@@ -3,12 +3,14 @@ import { CreateSeatController } from "../controllers/seat/create-seat.controller
 import { ListSeatsController } from "../controllers/seat/list-seats.controller";
 import { ReserveSeatController } from "../controllers/seat/reserve-seat.controller";
 import { ReleaseSeatController } from "../controllers/seat/release-seat.controller.ts";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const seatRoutes = Router();
 
-seatRoutes.post("/", new CreateSeatController().handle);
-seatRoutes.get("/", new ListSeatsController().handle);
-seatRoutes.post("/:id/reserve", new ReserveSeatController().handle);
+seatRoutes.post("/", [ensureAuthenticated, ensureAdmin], new CreateSeatController().handle);
+seatRoutes.get("/", [ensureAuthenticated], new ListSeatsController().handle);
+seatRoutes.post("/:id/reserve", [ensureAuthenticated], new ReserveSeatController().handle);
 seatRoutes.post("/:id/release", new ReleaseSeatController().handle);
 
 export default seatRoutes;
